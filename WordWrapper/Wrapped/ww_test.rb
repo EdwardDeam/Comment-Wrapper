0d28f21fce3b2e01eb46d4a57556f2d78eee6bba
EdwardDeam <45953948+EdwardDeam@users.noreply.github.com>
2019-02-23 11:30:37 +1100

    Initial Commit
    Added main Script. A File for testing, and the input and output folders

---
+++ WordWrapper/Wrapped/ww_test.rb
@@ -0,0 +1,14 @@
+line 1
+line 2
+line 3
+# comment 1
+# this line has too many characers, it will be chopped check that the word
+# 'submarine' is still intact!
+
+^ blank line
+# this line
+# this line has a long word or link
+# https://github.com/EdwardDeam/Comment-Wrapper/upload that should be moved to
+# its own line
+^ 126 char line
+# comment last
